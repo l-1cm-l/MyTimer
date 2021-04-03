5037d9e566ccf4bb21fb9e237505036b8d160ca0
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-04-03 09:08:46 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by administrator on 2021/04/03.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
