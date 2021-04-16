d97f42296434384b7689791069d6c9defc2a69b1
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-04-16 10:41:38 +0900

    [processing]

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timerHandler : Timer?
+    @state var count = 0
+    @AppStorage("timer_value") var timerValue = 10
     var body: some View {
         NavigationView{
-        VStack{Text("タイマー表示")}
+            ZStack{
+                Image("backgroundTimer")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                VStack(spacing:30.0){
+                    Text("残り10秒")
+                        .font(.largeTitle)
+                    HStack{
+                        Button(action: {})
+                        {
+                    Text("スタート")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 140, height: 140)
+                        .background(Color("startColor"))
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        }
+                        Button(action: {})
+                        {
+                    Text("ストップ")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 140, height: 140)
+                        .background(Color("stopColor"))
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                }
+            }
             .navigationBarItems(trailing:
             NavigationLink(
                 destination: SettingView())
--- MyTimer/SettingView.swift
+++ MyTimer/SettingView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State var timeValue = 10
     var body: some View {
-        Text("秒数設定")
+        ZStack{
+            Color("backgroudSetting")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+       
+            VStack{
+                Spacer()
+                Text("\(timeValue)秒")
+                    .font(.largeTitle)
+                Spacer()
+                Picker(selection: $timeValue, label: Text("選択")){
+                    Text("10").tag(10)
+                    Text("20").tag(30)
+                    Text("30").tag(30)
+                    Text("40").tag(40)
+                    Text("50").tag(50)
+                    Text("60").tag(60)
+                }
+              }
+            }
+        }
     }
-}
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
